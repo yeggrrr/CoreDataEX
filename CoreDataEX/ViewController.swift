651e0436518c9ee2d8232d0c4c64ba88398b2500
yeggrrr <yjkim981103@gmail.com>
2024-12-12 23:35:48 +0900

    Initial Commit

---
+++ CoreDataEX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataEX
+//
+//  Created by YJ on 12/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
