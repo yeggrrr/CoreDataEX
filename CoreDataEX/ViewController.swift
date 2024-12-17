714aca3a02d3875d6c5d2f8bae3b7435d480ea91
yeggrrr <yjkim981103@gmail.com>
2024-12-17 17:07:32 +0900

    feat: CoreData 저장 및 테이블뷰 실시간 반영

--- CoreDataEX/ViewController.swift
+++ CoreDataEX/ViewController.swift
@@ -21,7 +21,8 @@ class ViewController: UIViewController {
         
         configureTableView()
         configurePersistentContainer()
-        // createEntity()
+        configureAction()
+        fetchData()
     }
     
     private func configureTableView() {
@@ -35,12 +36,12 @@ class ViewController: UIViewController {
         self.container = appDelegate.persistentContainer
     }
     
-    private func createEntity() {
+    private func createEntity(name: String, gender: String, age: Int) {
         if let entity = NSEntityDescription.entity(forEntityName: "Yegr", in: self.container.viewContext) {
             let object = NSManagedObject(entity: entity, insertInto: self.container.viewContext)
-            object.setValue("Fubao", forKey: "name")
-            object.setValue("M", forKey: "gender")
-            object.setValue(4, forKey: "age")
+            object.setValue(name, forKey: "name")
+            object.setValue(gender, forKey: "gender")
+            object.setValue(age, forKey: "age")
         }
         
         saveData()
@@ -54,29 +55,50 @@ class ViewController: UIViewController {
         }
     }
     
-    private func fetchData() -> Int {
+    private func fetchData() -> [Yegr]? {
         do {
             let data = try self.container.viewContext.fetch(Yegr.fetchRequest())
-            return data.count
+            return data
         } catch {
             print("error!: \(error.localizedDescription)")
         }
         
-        return 0
+        return nil
+    }
+    
+    private func configureAction() {
+        coreDataView.saveButton.addTarget(self, action: #selector(saveButtonClicked), for: .touchUpInside)
+    }
+    
+    @objc func saveButtonClicked() {
+        createEntity(
+            name: coreDataView.nameTextField.text ?? "",
+            gender: coreDataView.genderTextField.text ?? "",
+            age: Int(coreDataView.ageTextField.text ?? "") ?? 0
+        )
+        
+        coreDataView.tableView.reloadData()
     }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 35
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fetchData()
+        guard let dataCount = fetchData()?.count else { return 0 }
+        return dataCount
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CoreDataCell.id, for: indexPath) as? CoreDataCell else { return UITableViewCell() }
         
-        cell.nameLabel.text = "Fubao"
-        cell.genderLabel.text = "M"
-        cell.ageLabel.text = "4"
+        if let data = fetchData() {
+            cell.nameLabel.text = data[indexPath.row].name
+            cell.genderLabel.text = data[indexPath.row].gender
+            cell.ageLabel.text = "\(data[indexPath.row].age)"
+        }
         
         return cell
     }
